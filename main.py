a5f703e01f580e19417fca11f6f74c7234c6826f
Alansaji2003 <117252157+Alansaji2003@users.noreply.github.com>
2022-12-13 19:15:32 +0530

    day 2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("Welcome to the Band name generator")
-a=input("What is the name of the city you grew up in?\n ")
-b=input("Whats your pet's name?\n ")
-print("Your band name could be "+a+" "+b)
+
+print("Welcome to the tip calculator")
+total_bill=int(input("What was the total bill? Rs "))
+prc_tip=int(input("What percentage tip would you like to give? 10,12 or 15 "))
+people=int(input("How many people to split the bill "))
+result=round(((total_bill*prc_tip/100)+total_bill)/people,2)
+#to make the result have 2 decimal place no matter what
+result="{:.2f}".format(result)
+print(f"Each person should pay {result} Rs")
