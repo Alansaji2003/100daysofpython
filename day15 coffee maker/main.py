4bca2d310b591accb2a989da2af83fcd15a7d622
Alan Saji <rockstaralansaji@gmail.com>
2023-02-01 22:56:35 +0530

    Update main.py

--- day15 coffee maker/main.py
+++ day15 coffee maker/main.py
@@ -28,6 +28,7 @@ resources = {
     "water": 300,
     "milk":  200,
     "coffee": 100,
+    "Money": 0,
 }
 
 
@@ -66,16 +67,34 @@ def check(x):
         else:
             return True
 
+profit = 0
+
 
 def count(y):
+    global profit
     print("Please insert coins")
     q = int(input("How many quarters "))
     d = int(input("How many dimes "))
     n = int(input("How many nickels "))
     p = int(input("How many pennies "))
     total = q*0.25 + d*0.1 + n*0.05 + p*0.01
+    profit += total
+    resources["Money"] = profit
     return total
 
+
+def use(x):
+    if x == "espresso":
+        resources["water"] = resources["water"] - MENU["espresso"]["ingredients"]["water"]
+        resources["coffee"] = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+    elif x == "latte":
+        resources["water"] = resources["water"] - MENU["latte"]["ingredients"]["water"]
+        resources["coffee"] = resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
+        resources["milk"] = resources["milk"] - MENU["latte"]["ingredients"]["milk"]
+    else:
+        resources["water"] = resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
+        resources["coffee"] = resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
+        resources["milk"] = resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
 
 
 OFF = False
@@ -93,12 +112,12 @@ while not OFF:
                 change = a - MENU["espresso"]["cost"]
                 print(f"Here is your change ${change}")
                 print("ENJOY YOUR ESPRESSO ☕")
+                use(choice)
             elif MENU["espresso"]["cost"] > a:
                 print(f"Sorry cash is not enough, here is your refund ${a}")
             else:
                 print("ENJOY YOUR ESPRESSO ☕")
-            resources["water"] = resources["water"] - MENU["espresso"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+
     elif choice == "latte":
         c = check(choice)
         if not c:
@@ -109,13 +128,12 @@ while not OFF:
                 change = a - MENU["latte"]["cost"]
                 print(f"Here is your change ${change}")
                 print("ENJOY YOUR LATTE ☕")
+                use(choice)
             elif MENU["latte"]["cost"] > a:
                 print(f"Sorry cash is not enough, here is your refund ${a}")
             else:
                 print("ENJOY YOUR LATTE ☕")
-            resources["water"] = resources["water"] - MENU["latte"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
-            resources["milk"] = resources["milk"] - MENU["latte"]["ingredients"]["milk"]
+
     elif choice == "cappuccino":
         c = check(choice)
         if not c:
@@ -126,13 +144,12 @@ while not OFF:
                 change = a - MENU["cappuccino"]["cost"]
                 print(f"Here is your change ${change}")
                 print("ENJOY YOUR CAPPUCCINO ☕")
+                use(choice)
             elif MENU["cappuccino"]["cost"] > a:
                 print(f"Sorry cash is not enough, here is your refund ${a}")
             else:
                 print("ENJOY YOUR CAPPUCCINO ☕")
-            resources["water"] = resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
-            resources["coffee"] = resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
-            resources["milk"] = resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
+
     elif choice == "off":
         print("MACHINE TURNED OFF")
         OFF = True
