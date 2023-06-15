5cb38842d1497f26c91f53d5e144e3463116c17c
Alan Saji <rockstaralansaji@gmail.com>
2023-06-15 17:58:24 +0530

    Delete main (1).py

--- main (1).py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band name generator")
-a=input("What is the name of the city you grew up in?\n ")
-b=input("Whats your pet's name?\n ")
-print("Your band name could be "+a+" "+b)
