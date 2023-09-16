a3c6db6e326271df82af8b277e8498d50780d253
Alan Saji <rockstaralansaji@gmail.com>
2023-09-16 20:40:06 +0530

    Update scoreboard.py
    high score retention update

--- day21 -snake game/scoreboard.py
+++ day21 -snake game/scoreboard.py
@@ -6,16 +6,31 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+
+        with open("score.txt", mode="r") as self.file:
+
+            self.highscore = self.file.read()
         self.color("white")
         self.penup()
         self.hideturtle()
-        self.goto(-80, 250)
-        self.write(f"Score : {self.score}", move=False, align="left", font=("Lilita One", 25, "normal"))
+        self.goto(-150, 250)
+        self.update_scoreboard()
 
     def increase(self):
         self.score += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score : {self.score}", move=False, align="left", font=("Lilita One", 25, "normal"))
-    def gameover(self):
-        self.goto(-70,0)
-        self.write("Game Over", move=False, align="left", font=("Lilita One", 25, "normal"))
+        self.write(f"Score : {self.score}    High Score: {self.highscore}", move=False, align="left", font=("Lilita One", 25, "normal"))
+
+    def reset(self):
+        if self.score > int(self.highscore):
+            self.highscore = self.score
+            with open("score.txt", mode="w") as self.file:
+                self.file.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+
+
