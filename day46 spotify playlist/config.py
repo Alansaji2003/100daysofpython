574ea55203a82d353fb22117a6d85218f17e8ed0
Alansaji2003 <rockstaralansaji@gmail.com>
2023-11-01 01:55:59 +0530

    day 46

---
+++ day46 spotify playlist/config.py
@@ -0,0 +1,7 @@
+class Config:
+    def __init__(self):
+        self.SPOTIFY_CLIENT_ID = "7ba7f538cfcd465aa85893abeadac14d"
+        self.SPOTIFY_CLIENT_SECRET = "44f3483813404e89939dfc6c507f9f3d"
+        self.REDIRECT_URI = "http://example.com"
+        self.OAUTH_AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+        self.OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
